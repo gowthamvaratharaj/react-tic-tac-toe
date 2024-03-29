b3b0d31e14733b78fa98895b077e7c6d735f9481
gowthamvaratharaj <gowthamversion0.1@gmail.com>
2024-03-29 14:15:24 +0530

    experimenting branch

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ function App() {
     let winner;
 
  for(const combination of WINNING_COMBINATIONS){
+  console.log("this Combination from APP : ",combination)
   const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
   const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
   const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
